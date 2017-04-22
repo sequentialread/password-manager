19c19ee8bd23839965a45dd6f21a066a9ce72eab
forest johnson <forest.n.johnson@gmail.com>
2017-04-22 12:00:16 -0500

    release 0.0.6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 GO15VENDOREXPERIMENT=1 go build -v -o sequentialread-password-manager server.go \
-  && docker build -t sequentialread/sequentialread-password-manager:0.0.3 .
+GOOS=linux GOARCH=amd64 go build -v -o sequentialread-password-manager server.go \
+  && docker build -t sequentialread/sequentialread-password-manager:0.0.6 .
